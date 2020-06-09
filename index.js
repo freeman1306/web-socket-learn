bf6003b58813678e2e3eeb8ca43049cbc9a18e7e
freeman1306 <zavalkin06@gmail.com>
2020-06-09 05:03:49 +0300

    done

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ var io = socket(server)
 io.on('connection', function(socket) {
     console.log('made socket connection', socket.id);
 
-
+//Handle cgat events
     socket.on('chat', function (data) {
         io.sockets.emit('chat', data)
     })
     
+
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data)
+    })
 }) 
--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
     output = document.getElementById('output');
+    feedback = document.getElementById('feedback')
       
 
 // Emit events
@@ -18,7 +19,18 @@ btn.addEventListener('click', function () {
 })
     
 
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value)
+})
+
+
 // Listen for events
 socket.on('chat', function (data) {
+    feedback.innerHTML = ''
     output.innerHTML += `<p> <strong> ${data.handle} : </strong> ${data.message}</p>`
+})
+
+
+socket.on('typing', function (data) {
+    feedback.innerHTML = `<p> <em> ${data} is typing a message... </em></p>`
 })
